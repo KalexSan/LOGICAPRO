da51d669ae533cffe394c6b52634d108d8da0233
Alex Sander <66784006+KalexSan@users.noreply.github.com>
2022-11-25 19:52:14 -0300

    aula

---
+++ aula1/funcoes.js
@@ -0,0 +1,12 @@
+function buscarMaiorMenor(){
+    if(i == 1){
+        maior = num;
+        menor = num;
+    }
+    if(num > maior){
+        maior = num;
+    }
+    if(num < menor){
+        menor = num;
+    }
+}
